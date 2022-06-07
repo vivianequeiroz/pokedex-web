c5f43731618354488d20192b11e1c26766b38990
João Bispo <joaobispo2077@gmail.com>
2022-06-06 23:01:03 -0300

    feat: consume navigation params in PokemonDetails page

--- src/pages/PokemonDetails/index.tsx
+++ src/pages/PokemonDetails/index.tsx
@@ -1,5 +1,14 @@
 import { FunctionComponent } from 'react';
 
+import { useParams } from 'react-router-dom';
+
+type PokemonDetailsParams = {
+  id: string;
+};
+
 export const PokemonDetails: FunctionComponent = () => {
+  const routeParams = useParams<PokemonDetailsParams>();
+  console.log(routeParams);
+
   return <h1>PokemonDetails!!!</h1>;
 };
