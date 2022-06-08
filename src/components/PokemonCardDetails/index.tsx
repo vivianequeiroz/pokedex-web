dbed930f8fcdff0ca4242c1aa4f9c38f7e279c73
João Bispo <joaobispo2077@gmail.com>
2022-06-07 22:27:47 -0300

    feat: add pokemon mocks

--- src/components/PokemonCardDetails/index.tsx
+++ src/components/PokemonCardDetails/index.tsx
@@ -10,8 +10,8 @@ export const PokemonCardDetails: FunctionComponent<PokemonCardDetailsProps> = ({
   pokemon,
 }) => {
   return (
-    <div className="flex flex-col justify-between items-center border-4 rounded-lg p-1 bg-green-400 gap-20 max-w-sm">
-      <header className="flex justify-between items-center p-4 border-b border-gray-800 w-full">
+    <div className="flex flex-col justify-between items-center rounded-lg p-1 bg-green-400 gap-20 max-w-sm">
+      <header className="flex justify-between items-center p-4 w-full">
         <div className="flex justify-between items-center gap-4">
           <img src={ArrowLeftIcon} alt="go back" className="h-4 text-white" />
           <h1 className="text-2xl font-bold text-white">{pokemon.name}</h1>
@@ -30,7 +30,7 @@ export const PokemonCardDetails: FunctionComponent<PokemonCardDetailsProps> = ({
           {pokemon.types.map((type) => (
             <li
               key={type.type.name}
-              className="bg-green-800 rounded-2xl py-1 px-3"
+              className="bg-green-800 rounded-2xl py-1 px-3 capitalize"
             >
               {type.type.name}
             </li>
@@ -46,11 +46,12 @@ export const PokemonCardDetails: FunctionComponent<PokemonCardDetailsProps> = ({
           <li>
             <span className="text-green-500 font-bold">HP</span>: 45
           </li>
-          <li>Attack: 49</li>
-          <li>Defense: 49</li>
-          <li>Sp. Attack: 65</li>
-          <li>Sp. Defense: 65</li>
-          <li>Speed: 45</li>
+          {pokemon.stats.map((stat) => (
+            <li key={stat.stat.name}>
+              <span className="text-green-500 font-bold">{stat.stat.name}</span>
+              : {stat.base_stat}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/pages/PokemonDetails/index.tsx
+++ src/pages/PokemonDetails/index.tsx
@@ -17,80 +17,82 @@ export const PokemonDetails: FunctionComponent = () => {
       <h2>
         <Logomark />
       </h2>
-      <PokemonCardDetails
-        pokemon={{
-          id: 1,
-          name: 'Bulbasaur',
-          image:
-            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-          stats: [
-            {
-              base_stat: 45,
-              effort: 0,
-              stat: {
-                name: 'hp',
-                url: 'https://pokeapi.co/api/v2/stat/1/',
+      <div className="flex flex-col justify-between items-center rounded-lg p-1 bg-white gap-20 max-w-sm">
+        <PokemonCardDetails
+          pokemon={{
+            id: 1,
+            name: 'Bulbasaur',
+            image:
+              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+            stats: [
+              {
+                base_stat: 45,
+                effort: 0,
+                stat: {
+                  name: 'hp',
+                  url: 'https://pokeapi.co/api/v2/stat/1/',
+                },
               },
-            },
-            {
-              base_stat: 49,
-              effort: 0,
-              stat: {
-                name: 'attack',
-                url: 'https://pokeapi.co/api/v2/stat/2/',
+              {
+                base_stat: 49,
+                effort: 0,
+                stat: {
+                  name: 'attack',
+                  url: 'https://pokeapi.co/api/v2/stat/2/',
+                },
               },
-            },
-            {
-              base_stat: 49,
-              effort: 0,
-              stat: {
-                name: 'defense',
-                url: 'https://pokeapi.co/api/v2/stat/3/',
+              {
+                base_stat: 49,
+                effort: 0,
+                stat: {
+                  name: 'defense',
+                  url: 'https://pokeapi.co/api/v2/stat/3/',
+                },
               },
-            },
-            {
-              base_stat: 65,
-              effort: 1,
-              stat: {
-                name: 'special-attack',
-                url: 'https://pokeapi.co/api/v2/stat/4/',
+              {
+                base_stat: 65,
+                effort: 1,
+                stat: {
+                  name: 'special-attack',
+                  url: 'https://pokeapi.co/api/v2/stat/4/',
+                },
               },
-            },
-            {
-              base_stat: 65,
-              effort: 0,
-              stat: {
-                name: 'special-defense',
-                url: 'https://pokeapi.co/api/v2/stat/5/',
+              {
+                base_stat: 65,
+                effort: 0,
+                stat: {
+                  name: 'special-defense',
+                  url: 'https://pokeapi.co/api/v2/stat/5/',
+                },
               },
-            },
-            {
-              base_stat: 45,
-              effort: 0,
-              stat: {
-                name: 'speed',
-                url: 'https://pokeapi.co/api/v2/stat/6/',
+              {
+                base_stat: 45,
+                effort: 0,
+                stat: {
+                  name: 'speed',
+                  url: 'https://pokeapi.co/api/v2/stat/6/',
+                },
               },
-            },
-          ],
-          types: [
-            {
-              slot: 1,
-              type: {
-                name: 'grass',
-                url: 'https://pokeapi.co/api/v2/type/12/',
+            ],
+            types: [
+              {
+                slot: 1,
+                type: {
+                  name: 'grass',
+                  url: 'https://pokeapi.co/api/v2/type/12/',
+                },
               },
-            },
-            {
-              slot: 2,
-              type: {
-                name: 'poison',
-                url: 'https://pokeapi.co/api/v2/type/4/',
+              {
+                slot: 2,
+                type: {
+                  name: 'poison',
+                  url: 'https://pokeapi.co/api/v2/type/4/',
+                },
               },
-            },
-          ],
-        }}
-      />
+            ],
+          }}
+        />
+      </div>
     </main>
   );
 };
